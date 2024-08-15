039aaf477a6fd62189de06b4c0cc9ff7d9dcb419
Gabriel Silva <gabiles278@gmail.com>
2024-08-15 10:45:30 -0300

    Config: configure envConfig service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { EnvConfigModule } from './shared/infastructure/env-config/env-config.module';
 @Module({
-	imports: [ConfigModule, EnvConfigModule],
+	imports: [EnvConfigModule],
 	controllers: [AppController],
 	providers: [AppService],
 })
