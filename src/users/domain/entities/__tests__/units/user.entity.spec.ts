19037dbc13d20e4398e03588456b3d63e7201f16
Gabriel Silva <gabiles278@gmail.com>
2024-08-21 05:07:46 -0300

    test<user.entity.spec.ts>: test gettters methods of UserEntity

--- src/users/domain/entities/__tests__/units/user.entity.spec.ts
+++ src/users/domain/entities/__tests__/units/user.entity.spec.ts
@@ -20,4 +20,29 @@ describe('UserEntity unit tests', () => {
 		expect(sut.props.password).toBe(props.password);
 		expect(sut.props.createdAt).toBeInstanceOf(Date);
 	});
+
+	it('Getter for name field', () => {
+		expect(sut.name).toBeDefined();
+		expect(sut.name).toBe(props.name);
+		expect(sut.name).toBeTypeOf('string');
+	});
+
+	it('Getter for email field', () => {
+		expect(sut.email).toBeDefined();
+		expect(sut.email).toBe(props.email);
+		expect(sut.email).toBeTypeOf('string');
+	});
+
+	it('Getter for password field', () => {
+		expect(sut.password).toBeDefined();
+		expect(sut.password).toBe(props.password);
+		expect(sut.password).toBeTypeOf('string');
+	});
+
+	it('Getter for email field', () => {
+		expect(sut.createdAt).toBeDefined();
+		expect(sut.createdAt).toBe(props.createdAt);
+		expect(sut.createdAt).toBeTypeOf('object');
+		expect(sut.createdAt).toBeInstanceOf(Date);
+	});
 });
